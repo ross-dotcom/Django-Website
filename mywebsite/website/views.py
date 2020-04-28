6a8c80e815c31667d4f718e042bb8495207744cc
ross-dotcom <wross843@gmail.com>
2020-04-28 22:27:42 +0200

    Made some additions and updates to the site.

--- mywebsite/website/views.py
+++ mywebsite/website/views.py
@@ -3,8 +3,17 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
     
+class AboutPageView(TemplateView):
+    template_name = 'about.html'    
+    
 class ProjectsPageView(TemplateView):
     template_name = 'projects.html'
     
 class WikiPageView(TemplateView):
-    template_name = 'wiki.html'    
+    template_name = 'wiki.html'
+    
+class BlogPageView(TemplateView):
+    template_name = 'blog.html'
+    
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
